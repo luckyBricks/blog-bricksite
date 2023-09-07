c7444d14d75b8a0a2107152d021e80dda0901127
Tao Sun <tao.sun@jetbrains.com>
2023-09-07 19:34:54 +0800

    update post cover source

--- layouts/layout.js
+++ layouts/layout.js
@@ -14,46 +14,6 @@ const PrismLoader = dynamic(() => import('@/components/PrismLoader'), {
   ssr: false,
 });
 
-// const Code = dynamic(() =>
-//   import('react-notion-x/build/third-party/code').then(async (m) => {
-//     // additional prism syntaxes
-//     await Promise.all([
-//       import('prismjs/components/prism-markup-templating.js'),
-//       import('prismjs/components/prism-markup.js'),
-//       import('prismjs/components/prism-bash.js'),
-//       import('prismjs/components/prism-c.js'),
-//       import('prismjs/components/prism-cpp.js'),
-//       import('prismjs/components/prism-csharp.js'),
-//       import('prismjs/components/prism-docker.js'),
-//       import('prismjs/components/prism-java.js'),
-//       import('prismjs/components/prism-js-templates.js'),
-//       import('prismjs/components/prism-coffeescript.js'),
-//       import('prismjs/components/prism-diff.js'),
-//       import('prismjs/components/prism-git.js'),
-//       import('prismjs/components/prism-go.js'),
-//       import('prismjs/components/prism-graphql.js'),
-//       import('prismjs/components/prism-handlebars.js'),
-//       import('prismjs/components/prism-less.js'),
-//       import('prismjs/components/prism-makefile.js'),
-//       import('prismjs/components/prism-markdown.js'),
-//       import('prismjs/components/prism-objectivec.js'),
-//       import('prismjs/components/prism-ocaml.js'),
-//       import('prismjs/components/prism-python.js'),
-//       import('prismjs/components/prism-reason.js'),
-//       import('prismjs/components/prism-rust.js'),
-//       import('prismjs/components/prism-sass.js'),
-//       import('prismjs/components/prism-scss.js'),
-//       import('prismjs/components/prism-solidity.js'),
-//       import('prismjs/components/prism-sql.js'),
-//       import('prismjs/components/prism-stylus.js'),
-//       import('prismjs/components/prism-swift.js'),
-//       import('prismjs/components/prism-wasm.js'),
-//       import('prismjs/components/prism-yaml.js'),
-//     ]);
-//     return m.Code;
-//   })
-// );
-
 const Collection = dynamic(() =>
   import('react-notion-x/build/third-party/collection').then(
     (m) => m.Collection
@@ -77,7 +37,7 @@ const Layout = ({
 }) => {
   const locale = useLocale();
   const router = useRouter();
-  const imgurlbkg = 'https://www.dmoe.cc/random.php';
+  const imgurlbkg = 'https://tuapi.eees.cc/api.php?category=fengjing&type=302';
   return (
     <Container
       layout="blog"
@@ -89,7 +49,7 @@ const Layout = ({
     >
       <article>
         <div
-          className="bg-fixed h-96 bg-center bg-contain flex flex-col items-start justify-end"
+          className="bg-fixed bg-no-repeat h-96 bg-center bg-contain flex flex-col items-start justify-end"
           style={{ backgroundImage: `url(${imgurlbkg})` }}
         >
           <div className="backdrop-blur-lg py-3 px-2 w-full">
